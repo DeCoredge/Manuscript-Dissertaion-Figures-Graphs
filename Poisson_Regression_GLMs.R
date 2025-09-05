b380840f13a7c447f141ab26e925dbc724570c97
DeCoreyJr <106572433+DeCoredge@users.noreply.github.com>
2025-09-05 10:56:54 -0400

    Update Poisson_Regression_GLMs.R

--- Poisson_Regression_GLMs.R
+++ Poisson_Regression_GLMs.R
@@ -1,11 +1,10 @@
-## Check the MiFish_Abundance_Poisson, it doesn't look right
 rm(list = ls())
 
 #Perform a Poisson Regression GLM using the abundance proportion of Fish species to their read counts in csv.
 edna_data<- read.csv("MiFish_Abundance_Poisson.csv", header = TRUE)
-edna_data$Abundance <- edna_data$Abundance # quick fix, eventually want to get actual values
-edna_data$MiFish <- edna_data$MiFish # quick fix, eventually want to get actual values
-edna_data$Leray <- edna_data$Leray # quick fix, eventually want to get actual values
+edna_data$Abundance <- edna_data$Abundance 
+edna_data$MiFish <- edna_data$MiFish
+edna_data$Leray <- edna_data$Leray
 
 #Replace N/A in csv dataset table.
 edna_data<- replace(edna_data, is.na(edna_data), "")
@@ -47,6 +46,68 @@ edna_data_02$Species <- as.factor(edna_data_02$Species)
 #Fit data into Poisson Regression GLMs.
 edna_Leray_model <- glm(Abundance ~ 1, data= edna_data_02, family = "poisson")
 edna_Leray_model_02 <- glm(Abundance ~ Leray, data= edna_data_02,
+                           family = "poisson")
+
+#View GLMs & Explore model fits and independent variables signifance
+summary(edna_Leray_model)
+summary(edna_Leray_model_02)
+
+# Add the model prediction to the plot
+plot(edna_data_02$Leray ~ edna_data_02$Abundance)
+abline(edna_Leray_model, col="green", lwd=2)
+abline(edna_Leray_model_02, col="purple", lwd=2)
+
+# Add the model prediction to the plot
+plot(log10(edna_data_02$Leray) ~ log10(edna_data_02$Abundance))
+abline(a=0, b=1) # one to one line
+
+#Perform a Multiplicative factor log-Poisson Regression GLM using the abundance proportion of all Invert. species to their read counts, and to their body type in csv.
+edna_data_02<- read.csv("Leray_Abundance_Poisson.csv", header = TRUE)
+edna_data_02$Abundance <- edna_data_02$Abundance
+edna_data_02$Leray <- edna_data_02$Leray
+edna_data_02$Body_Type <-edna_data_02$Body_Type
+
+#Replace N/A in csv dataset table.
+edna_data_02<- replace(edna_data_02, is.na(edna_data_02), "")
+
+#Fix any dataframe formating issues
+edna_data_02$Species <- as.factor(edna_data_02$Species)
+edna_data_02$Body_Type <- as.factor(edna_data_02$Body_Type)
+
+#Fit data into Poisson Regression GLMs.
+edna_Leray_model <- glm(Abundance ~ 1, data= edna_data_02, family = "poisson")
+edna_Leray_model_02 <- glm(Abundance ~ Leray*Body_Type, data= edna_data_02,
+                           family = "poisson")
+
+#View GLMs & Explore model fits and independent variables signifance
+summary(edna_Leray_model)
+summary(edna_Leray_model_02)
+
+# Add the model prediction to the plot
+plot(edna_data_02$Leray ~ edna_data_02$Abundance)
+abline(edna_Leray_model, col="green", lwd=2)
+abline(edna_Leray_model_02, col="purple", lwd=2)
+
+# Add the model prediction to the plot
+plot(log10(edna_data_02$Leray) ~ log10(edna_data_02$Abundance))
+abline(a=0, b=1) # one to one line
+
+#Perform a Additive factor log-Poisson Regression GLM using the abundance proportion of all Invert. species to their read counts, and to their body type in csv.
+edna_data_02<- read.csv("Leray_Abundance_Poisson.csv", header = TRUE)
+edna_data_02$Abundance <- edna_data_02$Abundance
+edna_data_02$Leray <- edna_data_02$Leray
+edna_data_02$Body_Type <-edna_data_02$Body_Type
+
+#Replace N/A in csv dataset table.
+edna_data_02<- replace(edna_data_02, is.na(edna_data_02), "")
+
+#Fix any dataframe formating issues
+edna_data_02$Species <- as.factor(edna_data_02$Species)
+edna_data_02$Body_Type <- as.factor(edna_data_02$Body_Type)
+
+#Fit data into Poisson Regression GLMs.
+edna_Leray_model <- glm(Abundance ~ 1, data= edna_data_02, family = "poisson")
+edna_Leray_model_02 <- glm(Abundance ~ Leray+Body_Type, data= edna_data_02,
                            family = "poisson")
 
 #View GLMs & Explore model fits and independent variables signifance
