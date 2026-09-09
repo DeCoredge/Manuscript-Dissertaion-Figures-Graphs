6eed5b82acb09ada83dc61c840a771bf9b3eff8e
DeCoreyJr <106572433+DeCoredge@users.noreply.github.com>
2026-09-09 19:38:19 -0400

    Updated Manuscript_Maps.R

--- Manuscript_Maps.R
+++ Manuscript_Maps.R
@@ -1,7 +1,9 @@
-#Map making for Manuscript & Dissertation
+#Sample Site Map making for Manuscript & Dissertation
 
 #Clear current working environment
 rm(list = ls())
+
+setwd("C:/Users/DeCorey Bolton Jr/Documents/GitHub/Manuscript-Dissertaion-Figures-Graphs")
 
 #Install Packages needed to make U.S. State Maps & Oceans
 install.packages(c("sf", "ggplot2", "usmap"))
@@ -26,7 +28,8 @@ lat1 <- 39.65 # Min. Latitude
 lat2 <- 46.02 # Max. Latitude
 
 #Get bathymetric data of Gulf of Maine 
-gom_bathy <- getNOAA.bathy(lon1 = lon1, lon2 = lon2, lat1 = lat1, lat2 = lat2, resolution = 5)
+gom_bathy <- getNOAA.bathy(lon1 = -71.1, lon2 = -62.28, lat1 = 39.65, 
+                           lat2 = 46.02, resolution = 1, keep = TRUE)
 
 # Convert bathymetric data to a dataframe to plot
 gom_bathy_df <- fortify(gom_bathy)
@@ -37,18 +40,21 @@ world <- ne_countries(scale = "medium", returnclass = "sf")
 #Plot the map
 base_map <- ggplot() +
   geom_raster(data = gom_bathy_df, aes(x = x, y = y, fill = z)) +
-  scale_fill_gradientn(colors = c("darkblue", "lightblue", "lightgreen", "darkgreen"),
-                       values = scales::rescale(c(min(gom_bathy_df$z), 0, max(gom_bathy_df$z))),
+  scale_fill_gradientn(colors = c( "darkblue", "blue", "skyblue"), limits = c(-1000, 0),
+                       oob = scales::squish,
                        name = "Depth (m)") +
-  geom_sf(data = world, fill = "gray", color = "black") + # Add coastline
+  geom_sf(data = world, fill = "#EADEC9", color = "black") + # Add coastline
   geom_contour(data = gom_bathy_df, aes(x = x, y = y, z = z), breaks = c(0, -100, -200), color = "darkgray", linetype = "dashed") + # Add bathymetric contours
   coord_sf(xlim = c(lon1, lon2), ylim = c(lat1, lat2), expand = FALSE) +
-  labs(title = "Paired eDNA-Trawl Sample Sites",
+  labs(title = "Paired eDNA - Trawl Sample Sites",
        x = "Longitude",
        y = "Latitude") +
   theme_minimal()
 
 base_map # check to make sure that the base map looks alright
+
+# check to make sure you/file are in the right directory
+file.exists("Maine_DMR_Trawl_Catch_Data.csv")
 
 #Use csv file to upload coordinates of sampling locations from trawls
 trawl_data <- read.csv("Maine_DMR_Trawl_Catch_Data.csv")
@@ -62,27 +68,16 @@ summary(trawl_data) # check that R read in the data frame right
 # Create a data frame for coordinates.
 trawl_df <- data.frame(id = c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
                               "K"),
-                       type = c("start", "end", "start", "end", "start", "end",
-                                "start", "end", "start", "end", "start", "end",
-                                "start", "end", "start", "end", "start", "end", 
-                                "start", "end", "start", "end"),
+                       type = c("sample site", "sample site", "sample site", 
+                                "sample site", "sample site", "sample site", 
+                                "sample site", "sample site", "sample site", 
+                                "sample site", "sample site"),
                        Latitude = c(43.461, 43.454, 43.488, 44.321, 44.486,
                                     43.487, 43.461, 43.455, 44.220, 44.421,
                                     44.483),
-                       Longitude = c(-69.836, , -69.898, -69.924, -67.559, 
+                       Longitude = c(-69.836, -69.898, -69.924, -67.559, 
                                      -67.506, -69.925, -69.837, -69.897, 
                                      -67.743, -67.445, -67.508))
-
-# Create a separate data frame for drawing lines
-lines_df <- data.frame(id = c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", 
-                              "K"),
-                      
-                    Start_Latitude = c(43.461, 43.454, 43.488, 44.321, 44.486,
-                                          43.487, 43.461, 43.455, 44.220, 44.421,
-                                          44.483),
-                    Start_Longitude = c(-69.836, -69.898, -69.924, -67.559,
-                                           -67.506, -69.925, -69.837, -69.897,
-                                           -67.743, -67.445, -67.508))
                       
 #Plot the points on the map w/ the scale of the map fixed to have the coordinate points more visible
 
@@ -95,4 +90,5 @@ base_map_zoomed <- base_map +
 #View New zoomed in Map
 base_map_zoomed
 print(base_map_zoomed)
+
 
